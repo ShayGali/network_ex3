a4ae0889b1ce8e434141b92d8382441e88483ad4
shayg <shaygali10@gmail.com>
2024-02-28 15:03:43 +0200

    add ip to the command line arguments

--- part A/TCP_Sender.c
+++ part A/TCP_Sender.c
@@ -9,22 +9,21 @@
 #include <time.h>
 #include <unistd.h>
 
-#include "args_parser.h"
-
-#define RECEIVER_IP "127.0.0.1"
 #define FILE_SIZE 2097152  // 2MB
 #define EXIT_MESSAGE "exit"
 
-int connect_to_recv(int port);
+int parse_args(int argc, char *argv[], char **ip, int *port, char **algo);
+int connect_to_recv(char *ip, int port);
 char *util_generate_random_data(unsigned int size);
 void send_exit_message(int sock);
 void send_file(int sock, char *file_data);
 
 int main(int argc, char *argv[]) {
   // Parse the command line arguments
+  char *ip;
   int port;
   char *algo;
-  if (!parse_args(argc, argv, &port, &algo)) {
+  if (!parse_args(argc, argv, &ip, &port, &algo)) {
     return 1;
   }
 
@@ -32,7 +31,7 @@ int main(int argc, char *argv[]) {
   char *file_data = util_generate_random_data(FILE_SIZE);
 
   // Create a TCP socket between the sender and the receiver
-  int sock = connect_to_recv(port);
+  int sock = connect_to_recv(ip, port);
   if (sock == -1) {
     return 1;
   }
@@ -80,7 +79,7 @@ char *util_generate_random_data(unsigned int size) {
  * The sender will connect to the receiver.
  * @return the socket file descriptor, -1 if an error occurred
  */
-int connect_to_recv(int port) {
+int connect_to_recv(char *ip, int port) {
   int sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock == -1) {
     printf("socket creation failed\n");
@@ -92,7 +91,7 @@ int connect_to_recv(int port) {
   receiver_addr.sin_family = AF_INET;    // IPv4
   receiver_addr.sin_port = htons(port);  // Convert port to network byte order
   // Convert IPv4 and IPv6 addresses from text to binary form
-  if (inet_pton(AF_INET, RECEIVER_IP, &(receiver_addr.sin_addr)) <= 0) {
+  if (inet_pton(AF_INET, ip, &(receiver_addr.sin_addr)) <= 0) {
     perror("invalid address or address not supported\n");
     return -1;
   }
@@ -135,4 +134,67 @@ void send_file(int sock, char *file_data) {
  */
 void send_exit_message(int sock) {
   send(sock, EXIT_MESSAGE, strlen(EXIT_MESSAGE), 0);
+}
+
+/**
+ * @brief Parse the port number from a string
+ * @return the port number if it is valid, -1 otherwise
+ */
+int parse_port(char *port) {
+  char *endptr;
+  long int port_num = strtol(port, &endptr, 10);
+  if (*endptr != '\0' || port_num < 0 || port_num > 65535) {
+    return -1;
+  }
+  return (int)port_num;
+}
+
+/**
+ * @brief Check if the algorithm is valid.
+ * a valid algorithm is either "reno" or "cubic"
+ * @return 1 if the algorithm is valid, 0 otherwise
+
+*/
+int is_valid_algo(char *algo) {
+  if (strcmp(algo, "reno") != 0 && strcmp(algo, "cubic") != 0) {
+    return 0;
+  }
+  return 1;
+}
+
+/**
+ * @brief Check if the arguments passed to the program are valid.
+ * @param argc the number of arguments
+ * @param argv the arguments passed to the program
+ * @param port a pointer to the port number. It will be updated if the port is
+ * valid
+ * @param algo a pointer to the algorithm. It will be updated if the algorithm
+ * is valid. a valid algorithm is either "reno" or "cubic"
+ * @return 1 if the arguments are valid, 0 otherwise
+ */
+int parse_args(int argc, char *argv[], char **ip, int *port, char **algo) {
+  if (argc != 7 ||  // check if the number of arguments is correct
+                    // check if the user passed the correct arguments
+      (strcmp(argv[1], "-ip") != 0 && strcmp(argv[3], "-p") != 0 &&
+       strcmp(argv[5], "-algo") != 0)) {
+    printf(
+        "Invalid arguments.\nUsage: %s -ip <IP> -p <port> -algo <algorithm>\n",
+        argv[0]);
+    return 0;
+  }
+
+  // parse the port number, and check if it is valid
+  // and store it in the port variable
+  *ip = argv[2];
+  *port = parse_port(argv[4]);
+  if (*port == -1) {
+    printf("Invalid port number\n");
+    return 0;
+  }
+  if (!is_valid_algo(argv[6])) {
+    printf("Invalid algorithm\n");
+    return 0;
+  }
+  *algo = argv[6];
+  return 1;
 }
--- part A/args_parser.h
+++
@@ -1,3 +0,0 @@
-int parse_args(int argc, char *argv[], int *port, char **algo);
-int parse_port(char *port);
-int is_valid_algo(char *algo);
