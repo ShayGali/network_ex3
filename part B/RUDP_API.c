6b2fd06e3628ad2c040ba5cbc1a14f1743541d86
hagaycohen2 <hagaycohen2@gmail.com>
2024-02-28 14:43:30 +0200

    Update RUDP_API.c with new functions and headers

--- part B/RUDP_API.c
+++ part B/RUDP_API.c
@@ -1,21 +1,37 @@
+#include "RUDP_API.h"
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #define WINDOW_MAX_SIZE 1024  // CHECK
 
-/*
-we will keep two variables to keep track of the window size
-1. base: the sequence number of the first byte in the window
-2. next: If the sender sent’s the packet, the variable next is incremented to 1
-*/
+int RUDP_socket(char *ip, int port) {
+  int send_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (send_socket == -1) {
+    printf("Could not create socket : %d", errno);
+    return -1;
+  }
+  struct sockaddr_in serverAddress;
+  memset(&serverAddress, 0, sizeof(serverAddress));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(port);
+  int rval = inet_pton(AF_INET, (const char *)ip, &serverAddress.sin_addr);
+  if (rval <= 0) {
+    printf("inet_pton() failed");
+    return -1;
+  }
+  return send_socket;
+}
 
-/*
-the timeouts will be handled using timers
-*/
-
-/*
-Packets are stored in a queue and each is assigned a value.
-This value represents the sum of the system's current time and the network delay
-
-When the current time of the system is associated with the assigned value the
-packet is freed from the queue and sent over the network.
-
-While the packet is sent over the network it gives an (ACK) acknowledgment
-*/
+int checksum(RUDP *packet) {
+  int sum = 0;
+  for (int i = 0; i < 10 && i < sizeof(packet->data); i++) {
+    sum += packet->data[i];
+  }
+  return sum;
+}
