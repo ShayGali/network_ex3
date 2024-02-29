a5cfa35b87c354f9207fbecaeb1e9f5a60251109
hagaycohen2 <hagaycohen2@gmail.com>
2024-02-29 11:08:59 +0200

    Add statistics tracking and error handling to RUDP_Receiver.c

--- part B/RUDP_Receiver.c
+++ part B/RUDP_Receiver.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "RUDP_API.h"
 
 int main(int argc, char* argv[]) {
@@ -9,9 +11,21 @@ int main(int argc, char* argv[]) {
     printf("Could not create socket\n");
     return -1;
   }
+  printf("Socket created\n");
+
+  FILE* file = fopen("stats", "w+");
+  if (file == NULL) {
+    printf("Error opening  stats file!\n");
+    return 1;
+  }
+  fprintf(file, "\n\n~~~~~~~~ Statistics ~~~~~~~~\n");
+  double average_time = 0;
+  double average_speed = 0;
+
   char* date_received;
   char* total_date;
   int rval = 0;
+  int run = 1;
   do {
     rval = RUDP_receive(socket, date_received, sizeof(date_received));
     if (rval == -1) {
@@ -20,6 +34,9 @@ int main(int argc, char* argv[]) {
     }
     memcpy(total_date + strlen(total_date), date_received,
            strlen(date_received));
+    if (strnlne(date_received) < 1024) {
+      printf("Received date: %s\n", total_date);
+    }
   } while (rval == 0);
 
   return 0;
