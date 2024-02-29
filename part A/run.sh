fee5555958b87d7f83f5a94f322e06857a5352c7
shayg <shaygali10@gmail.com>
2024-02-29 18:41:23 +0200

    Fix TCP_Sender algorithm typo

--- part A/run.sh
+++ part A/run.sh
@@ -4,6 +4,6 @@ make all
 echo "Running TCP_Reciever"
 ./TCP_Reciever -p 5050 -algo reno
 echo "Running TCP_Sender"
-./TCP_Sender -ip 127.0.0.1 -p 5050 -algo ren
+./TCP_Sender -ip 127.0.0.1 -p 5050 -algo reno
 
 make clean
