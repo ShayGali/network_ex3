ff6e3bff9062c1b31d653d4b99a16c1de627ef85
shayg <shaygali10@gmail.com>
2024-03-03 14:03:27 +0200

    Fix RUDP_Receiver buffer overflow issue

--- part B/RUDP_Reciever.c
+++ part B/RUDP_Reciever.c
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]) {
 
   char* date_received = NULL;
   int data_length = 0;
-  char* total_date = "";
+  char total_date[2097152] = {0};  // 2MB
   rval = 0;
   int run = 1;
 
@@ -41,23 +41,28 @@ int main(int argc, char* argv[]) {
   end = clock();
 
   do {
-    printf("Receiving data\n");
     rval = RUDP_receive(socket, &date_received, &data_length);
+
+    if (rval == -2) {  // if the connection was closed by the sender
+      break;
+    }
     if (rval <= 0) {
       printf("Error receiving data\n");
       return -1;
     }
-    if (rval == 1 &&
-        start <
-            end) {  // if the data received is the first one, start the timer
+    // printf("Data received: %s\n", date_received);
+
+    // if the data received is the first one, start the timer
+    if (rval == 1 && start < end) {
       start = clock();
     }
-    if (rval == 1) {  // if the data received is not the last one, add it to the
-                      // total data
+    // if the data received is not the last one, add it to the total data
+    if (rval == 1) {
       strcat(total_date, date_received);
     }
-    if (rval == 2) {  // if the data received is the last one, take it and write
-                      // the stats it to the file
+    // if the data received is the last one, take it and write the stats it to
+    // the file
+    if (rval == 2) {
       strcat(total_date, date_received);
       printf("Received date: %zu\n", sizeof(total_date));
       end = clock();
@@ -67,27 +72,26 @@ int main(int argc, char* argv[]) {
       average_speed += speed;
       fprintf(file, "Run #%d Data: Time=%f S ; Speed=%f MB/S\n", run,
               time_taken, speed);
-      total_date = "";
+      memset(total_date, 0, sizeof(total_date));
       run++;
     }
+
   } while (rval >= 0);  //   keep the loop until the connection is closed
 
-  if (rval ==
-      -2) {  // check if the connection was closed by the sender and print stats
-    printf("connection closed\n");
-    // add the average time and speed to the file
-    fprintf(file, "\n");
-    fprintf(file, "Average time: %f S\n", average_time / (run - 1));
-    fprintf(file, "Average speed: %f MB/S\n", average_speed / (run - 1));
+  // check if the connection was closed by the sender and print stats
+  printf("connection closed\n");
+  // add the average time and speed to the file
+  fprintf(file, "\n");
+  fprintf(file, "Average time: %f S\n", average_time / (run - 1));
+  fprintf(file, "Average speed: %f MB/S\n", average_speed / (run - 1));
 
-    // end the file with nice message
-    fprintf(file, "\n\n-----------------------------\n");
-    fprintf(file, "Thank you for using our RUDP service\n");
-    rewind(file);
-    char print_buffer[100];
-    while (fgets(print_buffer, 100, file) != NULL) {
-      printf("%s", print_buffer);
-    }
+  // end the file with nice message
+  fprintf(file, "\n\n-----------------------------\n");
+  fprintf(file, "Thank you for using our RUDP service\n");
+  rewind(file);
+  char print_buffer[100];
+  while (fgets(print_buffer, 100, file) != NULL) {
+    printf("%s", print_buffer);
   }
 
   // Close the file
